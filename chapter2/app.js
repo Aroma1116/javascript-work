c6d187eca617c06b0f37b69d6ba5bbcfc9b7e6d1
Aroma1116 <aroma1116@outlook.jp>
2022-08-17 08:04:17 +0000

    [Finish]chapter2

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,10 +1,8 @@
-let max = 100;
-let num = 1;
-let count = 0;
+let i;
+let num = 0;
 
-while (num < max)  {
-  num = num*2;
-  count = count + 1;
+for (i=1; i<11; i++){
+  num = num + i;
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert('1から10まで足し算した結果は' + num + 'です');
