456461d6b2edf9c0d3df7de3a392ad0530d4c827
Aroma1116 <aroma1116@outlook.jp>
2022-08-17 07:56:00 +0000

    while

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,10 +1,10 @@
-let orange = 100;
-let apple = 120;
+let max = 100;
+let num = 1;
+let count = 0;
 
-if (orange < apple){
-  alert("みかんの値段がリンゴより安い");
-} else if (orange == apple){
-  alert("みかんとリンゴが同じ値段");
-} else{
-  alert("みかんの値段がリンゴより高い");
+while (num < max)  {
+  num = num*2;
+  count = count + 1;
 }
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
