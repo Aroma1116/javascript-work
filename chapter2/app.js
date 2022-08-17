0fe55ac4d8ca424043a51b5fb29e0aa49ba2ae41
Aroma1116 <aroma1116@outlook.jp>
2022-08-17 07:53:25 +0000

    条件文

---
+++ chapter2/app.js
@@ -0,0 +1,10 @@
+let orange = 100;
+let apple = 120;
+
+if (orange < apple){
+  alert("みかんの値段がリンゴより安い");
+} else if (orange == apple){
+  alert("みかんとリンゴが同じ値段");
+} else{
+  alert("みかんの値段がリンゴより高い");
+}
