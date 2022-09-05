4322c0818f1b21a98312d984441cc40e52039d34
Aroma1116 <aroma1116@outlook.jp>
2022-09-05 04:52:45 +0000

    問題完了

--- chapter3/app.js
+++ chapter3/app.js
@@ -1,11 +1,17 @@
 let user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください。");
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert("グー・チョキ・パーのいずれかを入力してください");
+  user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください。");
+}
 
 let js_hand = getJShand();
 
 let judge = winLose(user_hand, js_hand);
-
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-
+if ((user_hand != null)){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+}else{
+  alert("またチャレンジしてね。");
+}
 function getJShand(){
   let js_hand_num = Math.floor(Math.random()*3)
   let hand_name;
