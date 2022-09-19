68a671b4b82398507271d450bcc823064d8200e7
Aroma1116 <aroma1116@outlook.jp>
2022-09-19 08:54:24 +0000

    Skippr

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$('.theTarget').skippr({
+  transition : 'fade',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  navType : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : true,
+  autoPlayDuration : 5000,
+  keyboardOnAlways : true,
+  hiiidePrevious : false,
+});
